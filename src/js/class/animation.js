7248730dcf993aa15ecd34c76e4e155df02b5939
AmirRezaDalir <dalirnet@gmail.com>
2018-04-07 12:06:41 +0430

    تلاش برای ساخت کلاس انیمیشن

---
+++ src/js/class/animation.js
@@ -0,0 +1,12 @@
+const helper = require("../helper");
+
+class animation {
+    constructor(scene) {
+
+        // init may var
+        this.scene = scene;
+
+    }
+}
+
+module.exports = animation;
